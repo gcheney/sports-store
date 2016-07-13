be519608426eceec317a3c398ea75b2b0c375251
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-13 11:18:27 -0500

    Comlete initial project setup

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repo;
+        public const int PageSize = 5;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repo = repository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel {
+                Products = repo.Products
+                    .OrderBy( p => p.ProductID)
+                    .Skip((page -1) * PageSize)
+                    .Take(PageSize), 
+                PagingInfo = new PagingInfo {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repo.Products.Count()
+                }
+            };
+
+            return View(model);
+        }
+	}
+}
